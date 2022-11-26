3502a16da11760a7511a3d5834356c0c4a70643e
Asutosh Acharya <asutosh246@gmail.com>
2022-11-26 18:35:35 +0530

    sending some html codes in response

--- Node practice/maxmillanUdemy/server.js
+++ Node practice/maxmillanUdemy/server.js
@@ -6,6 +6,14 @@ const http = require("http");
 //createServer returns server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<html>");
+  res.write("<head><title>My fist server</title> </head>");
+  res.write("<body><h1>My first node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
   //   process.exit();
 });
 
