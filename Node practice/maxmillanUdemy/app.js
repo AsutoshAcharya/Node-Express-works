3f42b02ee4c39934d71643a688b97af1598ddd82
AsutoshAcharya <asutosh246@gmail.com>
2022-12-09 22:08:21 +0530

    learning express from scratch

--- Node practice/maxmillanUdemy/app.js
+++ Node practice/maxmillanUdemy/app.js
@@ -1,12 +1,29 @@
 //require used to import something in js
 //path to your module starts with "./yourModule"
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//middlewares
+app.use(bodyParser.urlencoded({ extended: false })); //parser
+app.use("/add-products", (req, res, next) => {
+  // console.log("in users another middleware");
+  res.send(
+    `<form action="/product" method="POST"><input type="text" /><button type="submit">Add Product</button> </form>`
+  );
+});
+app.use("/product", (req, res, next) => {
+  // console.log("in  homepage middleware");
+  console.log(req.body);
+  res.redirect("/");
+});
+// const routes = require("./routes");
 // const { text } = require("stream/consumers");
 //called by node js every time a request is made to server
 
 //createServer returns server
 
-const server = http.createServer(routes.handler);
-
-server.listen(4000);
+app.listen(3000, () => {
+  console.log("server started at port 3000");
+});
