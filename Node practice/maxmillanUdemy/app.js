606504db69e80dea5890a5aaa1873024ff5bb7da
AsutoshAcharya <asutosh246@gmail.com>
2022-12-10 13:35:28 +0530

    adding 404 and add product pug files

--- Node practice/maxmillanUdemy/app.js
+++ Node practice/maxmillanUdemy/app.js
@@ -20,7 +20,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404");
 });
 
 app.listen(3000);
