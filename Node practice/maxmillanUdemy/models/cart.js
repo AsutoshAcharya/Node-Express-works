aabc80009e1b8340ed4dac5971aff711c39ff17a
Asutosh Acharya <asutosh246@gmail.com>
2024-04-29 20:10:12 +0530

    minor fix

--- Node practice/maxmillanUdemy/models/cart.js
+++ Node practice/maxmillanUdemy/models/cart.js
@@ -37,6 +37,9 @@ module.exports = class Cart {
       if (err) return;
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter((p) => p.id !== id);
       updatedCart.totalPrice =
