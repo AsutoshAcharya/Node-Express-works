ac6d72a3f4a681466cc43e178db5aa1bdf6adc00
AsutoshAcharya <asutosh246@gmail.com>
2024-05-10 12:06:01 +0530

    delete route fixed

--- Node practice/maxmillanUdemy/routes/task.js
+++ Node practice/maxmillanUdemy/routes/task.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { getTasks, addTask } = require("../controllers/task");
+const { getTasks, addTask, deleteTask } = require("../controllers/task");
 const router = express.Router();
 
 router.get("/getTasks", getTasks);
 
 router.post("/addTask", addTask);
 
-router.delete("/delete/:taskId", addTask);
+router.delete("/delete/:taskId", deleteTask);
 
 module.exports = router;
