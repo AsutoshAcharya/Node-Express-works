5a14a05dd11e74f3b1187496cc5b4f340397877d
AsutoshAcharya <asutosh246@gmail.com>
2024-05-13 18:31:01 +0530

    deleting cart products

--- Node practice/maxmillanUdemy/controllers/shop.js
+++ Node practice/maxmillanUdemy/controllers/shop.js
@@ -142,10 +142,20 @@ exports.postCart = (req, res, next) => {
 exports.deleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart
+        .getProducts({ where: { id: prodId } })
+        .then((products) => {
+          const product = products[0];
+          return product.cartItem.destroy();
+        })
+        .then((result) => {
+          res.redirect("/Cart");
+        });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
